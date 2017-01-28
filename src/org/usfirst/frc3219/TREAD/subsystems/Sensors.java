1261119dacb53291008f607ca48576f7d7f2ce3c
MaxGiovanniMacias <maxmacias123@yahoo.com>
2017-01-28 14:58:26 -0800

    Minor Sensor move

--- src/org/usfirst/frc3219/TREAD/subsystems/Sensors.java
+++ src/org/usfirst/frc3219/TREAD/subsystems/Sensors.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc3219.TREAD.subsystems;
 
+import org.usfirst.frc3219.TREAD.RobotMap;
+
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -9,13 +12,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Sensors extends Subsystem {
 	public I2C i2c;
 	public AHRS NAVX;
-
+public Encoder Encode;
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 		i2c = new I2C(I2C.Port.kMXP, 0x62);
 		NAVX = new AHRS(SPI.Port.kMXP);
-	}
+		Encode = RobotMap.driveEncoder;
+		Encode.setMaxPeriod(0.1);
+		Encode.setMinRate(10);
+	Encode.setDistancePerPulse(Math.PI/90);}
 
 	public int getDistance() {
 		byte[] buffer = new byte[2];
