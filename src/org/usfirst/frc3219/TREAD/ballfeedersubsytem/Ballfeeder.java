c64d00288e0b5e8cf9fa0bc56a6efa44b984dd97
MaxGiovanniMacias <maxmacias123@yahoo.com>
2017-01-30 17:45:44 -0800

    added methods to ballfeeder subystem

--- src/org/usfirst/frc3219/TREAD/ballfeedersubsytem/Ballfeeder.java
+++ src/org/usfirst/frc3219/TREAD/ballfeedersubsytem/Ballfeeder.java
@@ -1,5 +1,21 @@
 package org.usfirst.frc3219.TREAD.ballfeedersubsytem;
 
-public class Ballfeeder {
+import org.usfirst.frc3219.TREAD.RobotMap;
 
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Ballfeeder extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void TurnOnMotors() {
+		RobotMap.Motor.set(1);
+	}
+
+	public void TurnOffMotors() {
+		RobotMap.Motor.set(0);
+	}
 }
