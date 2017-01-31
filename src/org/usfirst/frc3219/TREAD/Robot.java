6c5c48fb65d0d76f48b1de3b26d20b41d152ac55
MaxGiovanniMacias <maxmacias123@yahoo.com>
2017-01-30 17:47:53 -0800

    added a construction subsystem method

--- src/org/usfirst/frc3219/TREAD/Robot.java
+++ src/org/usfirst/frc3219/TREAD/Robot.java
@@ -41,7 +41,7 @@ public class Robot extends IterativeRobot {
     	RobotMap.init();
     	
     	//Subsystem Construction, OI must be last.
-    	
+    	ballfeeder = new Ballfeeder();
         // OI must be constructed after subsystems. If the OI creates Commands
         //(which it very likely will), subsystems are not guaranteed to be
         // constructed yet. Thus, their requires() statements may grab null
