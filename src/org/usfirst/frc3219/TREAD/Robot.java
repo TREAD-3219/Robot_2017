6ef2ad30fba0273a9ba4d0cad2c97b9ea8fee042
MaxGiovanniMacias <maxmacias123@yahoo.com>
2017-01-30 16:51:15 -0800

    Made new joystick commands and new Climb subsytem

--- src/org/usfirst/frc3219/TREAD/Robot.java
+++ src/org/usfirst/frc3219/TREAD/Robot.java
@@ -8,10 +8,10 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
 package org.usfirst.frc3219.TREAD;
 
 import org.usfirst.frc3219.TREAD.commands.AutonomousCommand;
+import org.usfirst.frc3219.TREAD.subystem.Climber;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
@@ -27,74 +27,77 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  */
 public class Robot extends IterativeRobot {
 
-	//Command Declarations
-    Command autonomousCommand;
-    
-    //Subsystem Declarations
-    public static OI oi;
+	// Command Declarations
+	Command autonomousCommand;
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-    	RobotMap.init();
-    	
-    	//Subsystem Construction, OI must be last.
-    	
-        // OI must be constructed after subsystems. If the OI creates Commands
-        //(which it very likely will), subsystems are not guaranteed to be
-        // constructed yet. Thus, their requires() statements may grab null
-        // pointers. Bad news. Don't move it.
-        oi = new OI();
+	// Subsystem Declarations
+	public static OI oi;
+	public static Climber climber;
 
-        // instantiate the command used for the autonomous period
-        autonomousCommand = new AutonomousCommand();
-    }
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
+		RobotMap.init();
 
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
-    public void disabledInit(){
+		// Subsystem Construction, OI must be last.
+		climber = new Climber();
+		// OI must be constructed after subsystems. If the OI creates Commands
+		// (which it very likely will), subsystems are not guaranteed to be
+		// constructed yet. Thus, their requires() statements may grab null
+		// pointers. Bad news. Don't move it.
+		oi = new OI();
 
-    }
+		// instantiate the command used for the autonomous period
+		autonomousCommand = new AutonomousCommand();
+	}
 
-    public void disabledPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	/**
+	 * This function is called when the disabled button is hit. You can use it
+	 * to reset subsystems before shutting down.
+	 */
+	public void disabledInit() {
 
-    public void autonomousInit() {
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    public void teleopInit() {
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
+	public void autonomousInit() {
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null)
+			autonomousCommand.start();
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null)
+			autonomousCommand.cancel();
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
---
+++ src/org/usfirst/frc3219/TREAD/commands/Climb.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc3219.TREAD.commands;
+
+import org.usfirst.frc3219.TREAD.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Climb extends Command {
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		Robot.climber.TurnOffMotors();
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		Robot.climber.TurnOnMotors();
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
