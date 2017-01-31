29ec43ada23c7333fc8f8ff7722d7536d5d6388c
MaxGiovanniMacias <maxmacias123@yahoo.com>
2017-01-31 15:40:02 -0800

    Added a turn on motor command for ballfeed

--- src/org/usfirst/frc3219/TREAD/commands/Ballfeed.java
+++ src/org/usfirst/frc3219/TREAD/commands/Ballfeed.java
@@ -9,25 +9,25 @@ public class Ballfeed extends Command {
 	@Override
 	protected void end() {
 		// TODO Auto-generated method stub
-		Robot.ballfeeder.TurnOffMotors();	
+		Robot.ballfeeder.TurnOffMotors();
 	}
 
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
-		
+		Robot.ballfeeder.TurnOnMotors();
 	}
 
 	@Override
 	protected void interrupted() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
