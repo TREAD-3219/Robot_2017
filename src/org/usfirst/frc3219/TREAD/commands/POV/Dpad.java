41620532ef72e80937e7192e240907ede93497ad
Team TREAD developer login for Github <tread3219@gmail.com>
2017-02-18 14:12:25 -0800

    added dpad
    idk this is black magic

--- src/org/usfirst/frc3219/TREAD/commands/POV/Dpad.java
+++ src/org/usfirst/frc3219/TREAD/commands/POV/Dpad.java
@@ -1,13 +1,51 @@
 package org.usfirst.frc3219.TREAD.commands.POV;
 
-import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc3219.TREAD.Robot;
+import org.usfirst.frc3219.TREAD.RobotMap;
+import org.usfirst.frc3219.TREAD.subsystems.Turntable;
 
-public class Dpad extends Command {
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DPad extends Command {
+	
+	public DPad() {
+		requires(Robot.turntable);
+	}
+
+	@Override
+	protected void execute() {
+		int pov = Robot.oi.Gamecontroller.getPOV();
+		if (pov == 90) {
+			Robot.turntable.turnDirection(Turntable.TURNTABLE_FORWARD);
+		}
+
+		else if (pov == 270) {
+			Robot.turntable.turnDirection(Turntable.TURNTABLE_BACKWARD);
+		}
+
+		else {
+			Robot.turntable.turnDirection(0);
+		}
+	}
+
+	@Override
+	protected void end() {
+		Robot.turntable.turnDirection(0);
+	}
+
+	@Override
+	protected void initialize() {
+
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
-
 }
